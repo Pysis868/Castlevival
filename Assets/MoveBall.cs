0310441576cf8666a27b4f01387133c7875770b8
Edicitsep <Edicitsep@gmail.com>
2014-10-10 21:15:43 -0400

    Test

---
+++ Assets/MoveBall.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBall : MonoBehaviour {
+
+	float up = 0.0f;
+	float right = 0.0f;
+	float posX = 0.0f;
+	float posY = 0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey("a"))
+		{
+			right = -1.0f;
+		}
+		if (Input.GetKey("s"))
+		{
+			up = -1.0f;
+		}
+		if (Input.GetKey("w"))
+		{
+			up = 1.0f;
+		}
+		if (Input.GetKey("d"))
+		{
+			right = 1.0f;
+		}
+		posX = transform.position.x;
+		posY = transform.position.y;
+		transform.position.Set(posX + right, posY + up, 1.0f);
+	}
+}
